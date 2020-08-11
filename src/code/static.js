e2b45f68b2681b5f49147c6e91913d022c3b7cbb
LeJeu <64744459+le-jeu@users.noreply.github.com>
2020-08-11 19:42:39 +0200

    remove image part from statics

--- src/code/static.js
+++ src/code/static.js
@@ -14,20 +14,6 @@ skin.static = {
   CSS: {
     squirrel: require("./css/main.css")
   },
-  // the path to image files
-  // the file can be named anything so long as the property name is untouched
-  images: {
-    // toolbar_addlinks: require("./images/toolbar_addlinks.png"),
-    // toolbar_viewOps: require("./images/toolbar_viewOps.png"),
-    // toolbar_addMarkers: require("./images/toolbar_addMarkers.png"),
-    // toolbar_sync: require("./images/toolbar_sync.png"),
-    // toolbar_upload: require("./images/toolbar_upload.png"),
-    // toolbar_download: require("./images/toolbar_download.png"),
-    // toolbar_wasabeebutton_in: require("./images/toolbar_wasabeebutton_in.png"),
-    // toolbar_wasabeebutton_out: require("./images/toolbar_wasabeebutton_out.png"),
-    // toolbar_quickdraw: require("./images/toolbar_quickdraw.png"),
-    // toolbar_settings: require("./images/toolbar_settings.png")
-  },
 };
 
 // define any languages added by this skin here
@@ -35,131 +21,8 @@ skin.static = {
 skin.static.strings = {};
 skin.static.strings.squirrel = require("./strings.json");
 
-// define the images for markers here
-// the file name does not matter so long as the window.plugin.wasabee.static.constant.XXX and property names are untouched
-skin.static.markerTypes = new Map([
-  // [
-  //   "CapturePortalMarker",
-  //   {
-  //     markerIcon: require("./images/wasabee_markers_capture_pending.png"),
-  //     markerIconAssigned: require("./images/wasabee_markers_capture_assigned.png"),
-  //     markerIconAcknowledged: require("./images/wasabee_markers_capture_assigned.png"),
-  //     markerIconDone: require("./images/wasabee_markers_capture_done.png")
-  //   }
-  // ],
-  // [
-  //   "LetDecayPortalAlert",
-  //   {
-  //     markerIcon: require("./images/wasabee_markers_decay_pending.png"),
-  //     markerIconAssigned: require("./images/wasabee_markers_decay_assigned.png"),
-  //     markerIconAcknowledged: require("./images/wasabee_markers_decay_assigned.png"),
-  //     markerIconDone: require("./images/wasabee_markers_decay_done.png")
-  //   }
-  // ],
-  // [
-  //   "DestroyPortalAlert",
-  //   {
-  //     markerIcon: require("./images/wasabee_markers_destroy_pending.png"),
-  //     markerIconAssigned: require("./images/wasabee_markers_destroy_assigned.png"),
-  //     markerIconAcknowledged: require("./images/wasabee_markers_destroy_assigned.png"),
-  //     markerIconDone: require("./images/wasabee_markers_destroy_done.png")
-  //   }
-  // ],
-  // [
-  //   "FarmPortalMarker",
-  //   {
-  //     markerIcon: require("./images/wasabee_markers_farm_pending.png"),
-  //     markerIconAssigned: require("./images/wasabee_markers_farm_assigned.png"),
-  //     markerIconAcknowledged: require("./images/wasabee_markers_farm_assigned.png"),
-  //     markerIconDone: require("./images/wasabee_markers_farm_done.png")
-  //   }
-  // ],
-  // [
-  //   "GotoPortalMarker",
-  //   {
-  //     markerIcon: require("./images/wasabee_markers_goto_pending.png"),
-  //     markerIconAssigned: require("./images/wasabee_markers_goto_assigned.png"),
-  //     markerIconAcknowledged: require("./images/wasabee_markers_goto_assigned.png"),
-  //     markerIconDone: require("./images/wasabee_markers_goto_done.png")
-  //   }
-  // ],
-  // [
-  //   "GetKeyPortalMarker",
-  //   {
-  //     markerIcon: require("./images/wasabee_markers_key_pending.png"),
-  //     markerIconAssigned: require("./images/wasabee_markers_key_assigned.png"),
-  //     markerIconAcknowledged: require("./images/wasabee_markers_key_assigned.png"),
-  //     markerIconDone: require("./images/wasabee_markers_key_done.png")
-  //   }
-  // ],
-  // [
-  //   "CreateLinkAlert",
-  //   {
-  //     markerIcon: require("./images/wasabee_markers_link_pending.png"),
-  //     markerIconAssigned: require("./images/wasabee_markers_link_assigned.png"),
-  //     markerIconAcknowledged: require("./images/wasabee_markers_link_assigned.png"),
-  //     markerIconDone: require("./images/wasabee_markers_link_done.png")
-  //   }
-  // ],
-  // [
-  //   "MeetAgentPortalMarker",
-  //   {
-  //     markerIcon: require("./images/wasabee_markers_meetagent_pending.png"),
-  //     markerIconAssigned: require("./images/wasabee_markers_meetagent_assigned.png"),
-  //     markerIconAcknowledged: require("./images/wasabee_markers_meetagent_assigned.png"),
-  //     markerIconDone: require("./images/wasabee_markers_meetagent_done.png")
-  //   }
-  // ],
-  // [
-  //   "OtherPortalAlert",
-  //   {
-  //     markerIcon: require("./images/wasabee_markers_other_pending.png"),
-  //     markerIconAssigned: require("./images/wasabee_markers_other_assigned.png"),
-  //     markerIconAcknowledged: require("./images/wasabee_markers_other_assigned.png"),
-  //     markerIconDone: require("./images/wasabee_markers_other_done.png")
-  //   }
-  // ],
-  // [
-  //   "RechargePortalAlert",
-  //   {
-  //     markerIcon: require("./images/wasabee_markers_recharge_pending.png"),
-  //     markerIconAssigned: require("./images/wasabee_markers_recharge_assigned.png"),
-  //     markerIconAcknowledged: require("./images/wasabee_markers_recharge_assigned.png"),
-  //     markerIconDone: require("./images/wasabee_markers_recharge_done.png")
-  //   }
-  // ],
-  // [
-  //   "UpgradePortalAlert",
-  //   {
-  //     markerIcon: require("./images/wasabee_markers_upgrade_pending.png"),
-  //     markerIconAssigned: require("./images/wasabee_markers_upgrade_assigned.png"),
-  //     markerIconAcknowledged: require("./images/wasabee_markers_upgrade_assigned.png"),
-  //     markerIconDone: require("./images/wasabee_markers_upgrade_done.png")
-  //   }
-  // ],
-  // [
-  //   "UseVirusPortalAlert",
-  //   {
-  //     markerIcon: require("./images/wasabee_markers_virus_pending.png"),
-  //     markerIconAssigned: require("./images/wasabee_markers_virus_assigned.png"),
-  //     markerIconAcknowledged: require("./images/wasabee_markers_virus_assigned.png"),
-  //     markerIconDone: require("./images/wasabee_markers_virus_done.png")
-  //   }
-  // ],
-  // [
-  //   "ExcludeMarker",
-  //   {
-  //     markerIcon: require("./images/wasabee_markers_exclude_pending.png"),
-  //     markerIconAssigned: require("./images/wasabee_markers_exclude_assigned.png"),
-  //     markerIconAcknowledged: require("./images/wasabee_markers_exclude_assigned.png"),
-  //     markerIconDone: require("./images/wasabee_markers_exclude_done.png")
-  //   }
-  // ]
-]);
-
 // define the various colors here
 // https://leafletjs.com/reference-1.0.3.html#path
-// self-block doesn't need the "portal" data, but it doesn't hurt
 skin.static.layerTypes = new Map([
   // [
   //   "main",
@@ -172,9 +35,6 @@ skin.static.layerTypes = new Map([
   //       assignedDashArray: [4, 2, 1],
   //       opacity: 1,
   //       weight: 2
-  //     },
-  //     portal: {
-  //       iconUrl: require("./images/marker_layer_main.png")
   //     }
   //   }
   // ],
@@ -189,9 +49,6 @@ skin.static.layerTypes = new Map([
   //       assignedDashArray: [4, 2, 1],
   //       opacity: 1,
   //       weight: 2
-  //     },
-  //     portal: {
-  //       iconUrl: require("./images/marker_layer_groupa.png")
   //     }
   //   }
   // ],
@@ -206,9 +63,6 @@ skin.static.layerTypes = new Map([
   //       assignedDashArray: [4, 1, 4, 1],
   //       opacity: 1,
   //       weight: 2
-  //     },
-  //     portal: {
-  //       iconUrl: require("./images/marker_layer_groupb.png")
   //     }
   //   }
   // ],
@@ -223,9 +77,6 @@ skin.static.layerTypes = new Map([
   //       assignedDashArray: [4, 2, 1],
   //       opacity: 1,
   //       weight: 2
-  //     },
-  //     portal: {
-  //       iconUrl: require("./images/marker_layer_groupc.png")
   //     }
   //   }
   // ],
@@ -240,9 +91,6 @@ skin.static.layerTypes = new Map([
   //       assignedDashArray: [4, 2, 1],
   //       opacity: 1,
   //       weight: 2
-  //     },
-  //     portal: {
-  //       iconUrl: require("./images/marker_layer_groupd.png")
   //     }
   //   }
   // ],
@@ -257,9 +105,6 @@ skin.static.layerTypes = new Map([
   //       assignedDashArray: [4, 2, 1],
   //       opacity: 1,
   //       weight: 2
-  //     },
-  //     portal: {
-  //       iconUrl: require("./images/marker_layer_groupe.png")
   //     }
   //   }
   // ],
@@ -274,9 +119,6 @@ skin.static.layerTypes = new Map([
   //       assignedDashArray: [4, 2, 1],
   //       opacity: 1,
   //       weight: 2
-  //     },
-  //     portal: {
-  //       iconUrl: require("./images/marker_layer_groupf.png")
   //     }
   //   }
   // ],
@@ -291,9 +133,6 @@ skin.static.layerTypes = new Map([
   //       assignedDashArray: [4, 2, 1],
   //       opacity: 4,
   //       weight: 3
-  //     },
-  //     portal: {
-  //       iconUrl: require("./images/marker_layer_groupa.png")
   //     }
   //   }
   // ]
