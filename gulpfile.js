0754a58ce3b81b1d70d4dba06c12bbd0a68d1aa5
Scot Bontrager <scot@indievisible.org>
2020-08-11 16:46:07 -0500

    be platform independent

--- gulpfile.js
+++ gulpfile.js
@@ -98,10 +98,10 @@ gulp.task("buildplugin", cb => {
 });
 
 gulp.task("buildmeta", cb => {
-  const path = cfg.releaseFolder["prod"] + cfg.metaName;
+  const p = path.join(cfg.releaseFolder["prod"], cfg.metaName);
 
-  ensureDirectoryExistence(path);
-  fs.writeFile(path, status.headers, err => {
+  ensureDirectoryExistence(p);
+  fs.writeFile(p, status.headers, err => {
     cb(err);
   });
 });
